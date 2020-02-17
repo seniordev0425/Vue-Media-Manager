502b782fb3e1de528b418b1c74ca256be091222a
SeniorDev0425 <senior.dev0425@outlook.com>
2020-02-16 16:47:13 -0800

    Update AuthService

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -16,6 +16,8 @@ class AuthServiceProvider extends ServiceProvider
         'App\Model' => 'App\Policies\ModelPolicy',
     ];
 
+
+
     /**
      * Register any authentication / authorization services.
      *
